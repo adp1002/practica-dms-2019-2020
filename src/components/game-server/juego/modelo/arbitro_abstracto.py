d2841bfe3810d6a3e77ecbc31a1f9cc19752d428
adp1002 <adp1002@alu.ubu.es>
2019-10-28 21:07:04 +0100

    Creado clase arbitro_tres_raya

--- src/components/game-server/juego/modelo/arbitro_abstracto.py
+++ src/components/game-server/juego/modelo/arbitro_abstracto.py
@@ -1,12 +1,47 @@
 from abc import ABC, abstractmethod
 
 class ArbitroAbstracto(ABC):
+    """Clase abstracta que representa las reglas del juego.
+    ---
+    La clase proporciona la estructura de las reglas del juego.
+    """
 
     @abstractmethod
     def jugar(self, x, y):
+        """Método que realiza un movimiento.
+        ---
+            Parámetros:
+                - x: Fila del tablero.
+                - y: Columna del tablero.
+        """
         pass
-    
+
     @abstractmethod
     def es_valido(self, x, y):
+        """Método que comprueba la jugada.
+        ---
+            Parámetros:
+                - x: Fila del tablero.
+                - y: Columna del tablero.
+            Returns:
+                True si el movimiento es válido, sino False.
+        """
         pass
-    
+
+    @abstractmethod
+    def obtener_ganador():
+        """Método que decide si la partida ha terminado.
+        ---
+            Returs:
+                Un Jugador con el ganador.
+        """
+        pass
+
+    @abstractmethod
+    def obtener_turno():
+        """Método que obtiene el jugador con el turno.
+        ---
+            Returns:
+                Jugador con el turno.
+        """
+        pass
