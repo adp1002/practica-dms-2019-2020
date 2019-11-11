62b7e760b948b11d74bbbd2feefa1d3753a37045
adp1002 <adp1002@alu.ubu.es>
2019-11-11 20:25:55 +0100

    Completados arbitros

--- src/components/game-server/juego/control/arbitro_abstracto.py
+++ src/components/game-server/juego/control/arbitro_abstracto.py
@@ -3,10 +3,10 @@ from abc import ABC, abstractmethod
 class ArbitroAbstracto(ABC):
 
     def __init__(self, tablero):
-        self.__tablero = tablero
+        self._tablero = tablero
 
     def es_valido(self, x, y):
-        return self.__tablero.obtener_pieza(x, y) != None
+        return self._tablero.obtener_pieza(x, y) != None
 
     def esta_acabado(self):
-        return self.__tablero.esta_lleno()
+        return self._tablero.esta_lleno()
--- src/components/game-server/tres_raya/control/arbitro_tres_raya.py
+++ src/components/game-server/tres_raya/control/arbitro_tres_raya.py
@@ -6,8 +6,15 @@ class ArbitroTresRaya(ArbitroAbstracto):
         super(ArbitroTresRaya, self).__init__(tablero)
 
     def esta_acabado(self):
+        t = self._tablero
+        return super.esta_acabado() or \
+        t.obtener_pieza(0,0) == t.obtener_pieza(0,1) == t.obtener_pieza(0,2) or \
+        t.obtener_pieza(1,0) == t.obtener_pieza(1,1) == t.obtener_pieza(1,2) or \
+        t.obtener_pieza(2,0) == t.obtener_pieza(2,1) == t.obtener_pieza(2,2) or \
 
-        if super.esta_acabado():
-            return True
+        t.obtener_pieza(0,0) == t.obtener_pieza(1,0) == t.obtener_pieza(2,0) or \
+        t.obtener_pieza(0,1) == t.obtener_pieza(1,1) == t.obtener_pieza(2,1) or \
+        t.obtener_pieza(0,2) == t.obtener_pieza(1,2) == t.obtener_pieza(2,2) or \
 
-        
+        t.obtener_pieza(0,0) == t.obtener_pieza(1,1) == t.obtener_pieza(2,2) or \
+        t.obtener_pieza(0,2) == t.obtener_pieza(1,1) == t.obtener_pieza(2,0)
