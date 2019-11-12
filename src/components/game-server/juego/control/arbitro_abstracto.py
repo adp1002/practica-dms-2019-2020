1a73884a81ee4f23ec4d152191a665005d523945
adp1002 <adp1002@alu.ubu.es>
2019-11-12 21:08:57 +0100

    Documentación y arreglos.

--- src/components/game-server/juego/control/arbitro_abstracto.py
+++ src/components/game-server/juego/control/arbitro_abstracto.py
@@ -3,9 +3,20 @@ from abc import ABC, abstractmethod
 class ArbitroAbstracto(ABC):
 
     def __init__(self, tablero):
+        """ Constructor
+        ---
+        Inicializa un arbitro con el tablero de la partida.
+
+        Parametros:
+            - tablero: tablero de la partida.
+        """
         self._tablero = tablero
 
     def es_valido(self, x, y):
+        """
+        ---
+
+        """
         return self._tablero.obtener_pieza(x, y) != None
 
     @abstractmethod
--- src/components/game-server/tres_raya/control/arbitro_tres_raya.py
+++ src/components/game-server/tres_raya/control/arbitro_tres_raya.py
@@ -3,21 +3,40 @@ from juego.control.arbitro_abstracto import ArbitroAbstracto
 class ArbitroTresRaya(ArbitroAbstracto):
 
     def __init__(self, tablero):
+        """ Constructor
+        ---
+        Llama al constructor de la superclase.
+
+        Parametros:
+            - tablero: tablero de la partida.
+        """
         super(ArbitroTresRaya, self).__init__(tablero)
 
     def hay_ganador(self):
+        """
+        ---
+        Comprueba si existe un ganador comprobando todas las posibles
+        combinaciones ganadoras.
+
+        Returns: True si hay un ganador, False en caso contrario.
+        """
         t = self._tablero
         return \
         t.obtener_pieza(0,0) == t.obtener_pieza(0,1) == t.obtener_pieza(0,2) or \
         t.obtener_pieza(1,0) == t.obtener_pieza(1,1) == t.obtener_pieza(1,2) or \
         t.obtener_pieza(2,0) == t.obtener_pieza(2,1) == t.obtener_pieza(2,2) or \
-
         t.obtener_pieza(0,0) == t.obtener_pieza(1,0) == t.obtener_pieza(2,0) or \
         t.obtener_pieza(0,1) == t.obtener_pieza(1,1) == t.obtener_pieza(2,1) or \
         t.obtener_pieza(0,2) == t.obtener_pieza(1,2) == t.obtener_pieza(2,2) or \
-
         t.obtener_pieza(0,0) == t.obtener_pieza(1,1) == t.obtener_pieza(2,2) or \
         t.obtener_pieza(0,2) == t.obtener_pieza(1,1) == t.obtener_pieza(2,0)
 
     def esta_acabado(self):
-        return self.hay_ganador() || self._tablero.esta_lleno()
+        """
+        ---
+        Comprueba si la partida ha finalizado, sea porque hay un ganador o
+        porque el tablero está lleno.
+
+        Returns: True si la partida ha terminado, False en caso contrario.
+        """
+        return self.hay_ganador() or self._tablero.esta_lleno()
