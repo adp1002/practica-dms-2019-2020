f562bca28d5f93f2f762b31a0176ac24f85eb457
ssr1002 <44766319+SSR1002@users.noreply.github.com>
2019-11-19 20:00:08 +0100

    Actualizar cliente

--- src/components/client/client.py
+++ src/components/client/client.py
@@ -9,19 +9,20 @@ Returns:
 """
 def seleccion_de_servidor():
     print('Listando servidores activos...\n')
-    peticion_repetir = 'S'
+    peticion_repetir = '0'
     #Bucle para pedir servidores
     while(peticion_repetir not in ['N','n']):
         servidores_activos = HubClient.instance().get_servers(token)
+
+        #Si no hay servidores activos, se puede repetir la busqueda o salir
         if(servidores_activos is None or not servidores_activos):
-            #Si no hay servidores activos, se repite la busqueda o se sale
             print('¡No hay servidores de juego activos!')
-            peticion_repetir = input('¿Quieres repetir la busqueda? (S/exit): ')
-            if(peticion_repetir in ['N','n']):
-                print('Para jugar necesitar un servidor.')
-                peticion_repetir = 'S'
-            elif(peticion_repetir == 'exit'):
-                exit()
+            while(peticion_repetir not in ['S','s','exit']):
+                peticion_repetir = input('¿Quieres repetir la busqueda? (S/exit): ')
+                if(peticion_repetir == 'exit'):
+                    exit()
+
+        #Listamos los servidores
         else:
             for idx,servidor in enumerate(servidores_activos):
                 print(str(idx) + ". " + servidor['name'] + " (" + servidor['host'] + ":" + servidor['port'] + ")")
@@ -38,20 +39,21 @@ def seleccion_de_servidor():
                 exit()
     return servidores_activos[int(servidor_elegido)]
 
+
 """ Registra al usuario actual en el servidor pasado por parametro
 ---
 Parameters:
     - servidor_seleccionado: Array con la informacion del servidor seleccionado para registrar al usuario
 Returns:
-    True si se ha registrado correctamente, si no, False
+    El servidor de juego o None si ha surgido un error.
 """
 def registrarse_en_servidor(servidor_seleccionado):
     servidor_de_juego = GameClient(servidor_seleccionado['host'],servidor_seleccionado['port'])
     if((servidor_de_juego is None) or (not servidor_de_juego.registrar_usuario_en_servidor(token))):
         print('\nHa ocurrido un error al intentar entrar al servidor. Intenta probar en un nuevo servidor.\n')
-        return False
+        return None
     else:
-        return True
+        return servidor_de_juego
 
 """ Dibuja una matriz 
 ---
@@ -68,9 +70,18 @@ def dibujar_tablero(tablero):
 
 
 
+
+
+
+
+
 '''Comienzo del script'''
-#Obtenemos los datos del usuario
+'''
+Registro y/o Login del usuario
+'''
+#Inicializamos el token del jugador a None
 token = None
+
 while(token is None):
     username = input('Introduce el nombre del usuario:\n')
     password = input('Introduce la contraseña del usuario:\n')
@@ -85,27 +96,38 @@ while(token is None):
         print('Usuario autentificado correctamente.')
 
 
+'''
+Listado y seleccion de los servidores de juego
+'''
+#Inicializamos el servidor de juego a None
+servidor_de_juego = None
 
-#Seleccionamos el servidor con la funcion seleccion_de_servidor
-servidor_seleccionado = seleccion_de_servidor()
-
-#Registramos al usuario en servidor. Si no puede, volvemos a mostrar la lista de servidores.
-while(not registrarse_en_servidor(servidor_seleccionado)):
+while(servidor_de_juego is None):
+    #Seleccionamos el servidor con la funcion seleccion_de_servidor
     servidor_seleccionado = seleccion_de_servidor()
+    #Registramos al usuario en servidor. Si no puede, volvemos a mostrar la lista de servidores.
+    servidor_de_juego = registrarse_en_servidor(servidor_seleccionado))
 
+
+'''
+Juego en el cliente
+'''
 #Comenzamos el bucle de pedir tablero y jugar
 while(not servidor_de_juego.esta_acabado()):
+
+    #Dibujo del tablero de esta iteraccion
     tablero_actual = servidor_de_juego.get_tablero()
-    if(tablero_actual is None):
+    if(tablero_actual):
+        dibujar_tablero(tablero_actual)
+    else:
         print("\nError al obtener el tablero. Abortando cliente...\n")
         exit()
-    else:
-        dibujar_tablero(tablero_actual)
     
-    #Comprobamos si es nuestro turno.
+    #Comprobamos si es nuestro turno y jugamos
     nuestro_turno = servidor_de_juego.get_turno(token)
     if(nuestro_turno):
         print('\n¡Tu turno!\n')
+        #Bucle para asegurarse de que la jugada es aceptada por servidor
         es_jugada_valida = False
         while(not es_jugada_valida):
             coor_x = input('Introduce la cordenada x: ')
@@ -116,11 +138,16 @@ while(not servidor_de_juego.esta_acabado()):
         #Volvemos a dibujar el tablero despues de jugar
         tablero_actual = servidor_de_juego.get_tablero()
         dibujar_tablero(tablero_actual)
+
     else:
         print('\nAun no es tu turno.\n')
-        
-        input('Pulsa una tecla para actualizar.')
 
+    #Pauser bucle hasta nuevo input.    
+    input('Pulsa una tecla para actualizar.')
+
+'''
+Fin de partida. Mostrando resultados.
+'''
 print('¡Partida finalizada! Mostrando el tablero final:')
 tablero_actual = servidor_de_juego.get_tablero()
 dibujar_tablero(tablero_actual)
